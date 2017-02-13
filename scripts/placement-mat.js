d1a13a1d129926beafffd11b5c9ef5eb3bd19da3
lcb931023 <lcb931023@gmail.com>
2017-02-12 17:08:05 -0800

    quick test for synth

--- scripts/placement-mat.js
+++ scripts/placement-mat.js
@@ -3,8 +3,11 @@
 AFRAME.registerComponent('placement-mat', {
   init: function () {
     this.el.addEventListener('click', function (e) {
-      console.log('I was clicked at: ', e.detail.intersection.point);
-      console.log(e);
+      console.log(e.detail.intersection.point);
+      var point = e.detail.intersection.point;
+      var freq = (point.y + 20)*40;
+      var vol = (point.x+5)*0.1
+      synth.generateTone(freq, 1, vol, 0);
     });
   }
 });
