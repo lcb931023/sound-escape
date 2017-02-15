410541a8355adeff86a8a8caf69dac44729ec08a
lcb931023 <lcb931023@gmail.com>
2017-02-14 19:36:46 -0800

    generate sphere on click

--- scripts/placement-mat.js
+++ scripts/placement-mat.js
@@ -5,9 +5,19 @@ AFRAME.registerComponent('placement-mat', {
     this.el.addEventListener('click', function (e) {
       console.log(e.detail.intersection.point);
       var point = e.detail.intersection.point;
-      var freq = (point.y + 20)*40;
-      var vol = (point.x+5)*0.1
-      synth.generateTone(freq, 1, vol, 0);
+      
+      // var freq = (point.y + 20)*40;
+      // var vol = (point.x+5)*0.1
+      // synth.generateTone(freq, 1, vol, 0);
+      // NOTE no easy way to make placement mat invisible AND clickable via cursor
+      // TODO find a better way to register 3D position and generate sphere
+      
+      var sphere = document.createElement('a-sphere');
+      sphere.setAttribute('color', '#FFFFFF');
+      sphere.setAttribute('radius', '0.1');
+      sphere.setAttribute('position', `${point.x} ${point.y} ${point.z}`);
+      var scene = document.querySelector('a-scene');
+      scene.appendChild(sphere);
     });
   }
 });
