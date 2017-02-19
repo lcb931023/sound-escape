6f2c244ab25a174795f05aebee3af545168a18bf
lcb931023 <lcb931023@gmail.com>
2017-02-18 23:33:20 -0800

    tonight we feast on orbs!

--- scripts/placement-mat.js
+++ scripts/placement-mat.js
@@ -3,14 +3,10 @@
 AFRAME.registerComponent('placement-mat', {
   init: function () {
     this.el.addEventListener('click', function (e) {
-      console.log(e.detail.intersection.point);
       var point = e.detail.intersection.point;
-      
       // var freq = (point.y + 20)*40;
       // var vol = (point.x+5)*0.1
       // synth.generateTone(freq, 1, vol, 0);
-      // NOTE no easy way to make placement mat invisible AND clickable via cursor
-      // TODO find a better way to register 3D position and generate sphere
       
       var sphere = document.createElement('a-sphere');
       sphere.setAttribute('color', '#FFFFFF');
