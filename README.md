f282804939fdf0a1c6916ec6033d5530f4b592ba
Changbai Li <changbai.li@heliosinteractive.com>
2017-03-07 02:44:50 -0800

    patch mouse cursor to send mousedown and touchstart event

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # sound-escape
+
+## Current Goal
+
+Creating: Tap the screen to create orbs
+
+[ ] the longer you hold, the bigger the orb is
+[ ] drag to move orb
+[ ] press on existing orb to remove it
+[ ] periodically send out a wave to trigger the orbs
+  - sample the positions of the orb to play, and then supply matching visuals
+  - flat plane from camera
+  - omni-directional wave sphere
+[ ] orb played indicator
+  - flashes color when it gets played
+
+[ ] refactor mouse cursor to emit mousedown, mouseup, touchstart, touchend event
+[ ] 
+
+Playing: Trigger the sounds by some kind of interaction that works on mobile phone.
+
+Audio: Based on
--- scripts/placement-mat.js
+++ scripts/placement-mat.js
@@ -1,19 +1,23 @@
 /* A plane we use to place sound spheres on */
 
 AFRAME.registerComponent('placement-mat', {
+  pressHandler: function (e) {
+    console.log(e);
+    // handles the press better than white house
+    var point = e.detail.intersection.point;
+    // var freq = (point.y + 20)*40;
+    // var vol = (point.x+5)*0.1
+    // synth.generateTone(freq, 1, vol, 0);
+
+    var sphere = document.createElement('a-sphere');
+    sphere.setAttribute('color', '#FFFFFF');
+    sphere.setAttribute('radius', '0.1');
+    sphere.setAttribute('position', `${point.x} ${point.y} ${point.z}`);
+    var scene = document.querySelector('a-scene');
+    scene.appendChild(sphere);
+  },
   init: function () {
-    this.el.addEventListener('click', function (e) {
-      var point = e.detail.intersection.point;
-      // var freq = (point.y + 20)*40;
-      // var vol = (point.x+5)*0.1
-      // synth.generateTone(freq, 1, vol, 0);
-      
-      var sphere = document.createElement('a-sphere');
-      sphere.setAttribute('color', '#FFFFFF');
-      sphere.setAttribute('radius', '0.1');
-      sphere.setAttribute('position', `${point.x} ${point.y} ${point.z}`);
-      var scene = document.querySelector('a-scene');
-      scene.appendChild(sphere);
-    });
+    this.el.addEventListener('mousedown', this.pressHandler);
+    this.el.addEventListener('touchstart', this.pressHandler);
   }
 });
